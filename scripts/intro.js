201ed2e6031c756579e378c5f66722d7ad1fa6ab
munizeduardo <63055965+munizeduardo@users.noreply.github.com>
2020-06-02 11:58:35 -0300

    teste2

---
+++ scripts/intro.js
@@ -0,0 +1,64 @@
+// variaveis - guardar dados em memória
+let myvar = "He" // variaveis podem ser declaradas com "var" ou "let"
+const myconst = "He" // constante
+
+// string
+// "Isso é uma string"
+// "Isso também é um string"
+// "Isso é uma string também"
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bTrue)
+
+// objetos possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// // Registrar função
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Kyan")
+
+// condicionais
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ){
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+  
